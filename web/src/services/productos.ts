11033ac11f8711ba4d66710ab5459d6e1c211102
Manuel <manutejadaparonetto@gmail.com>
2026-06-02 13:48:53 -0300

    cambios en desc
    cambio:
    `/api/producto/${ean}`,
    por
          `/api/productos/${ean}`,
    xd.
    Y new PrismaClient() necesitaba un adapter

--- web/src/services/productos.ts
+++ web/src/services/productos.ts
@@ -43,7 +43,7 @@ export async function fetchProductoPorEAN(
 
   const response =
     await fetch(
-      `/api/producto/${ean}`,
+      `/api/productos/${ean}`,
       {
         cache: 'no-store',
       }
